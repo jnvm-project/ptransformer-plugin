303c531a36688242274186a6305ee322dcdc02d9
kbamponsem <kwabenaamponsem@gmail.com>
2021-02-17 15:43:35 +0100

    add README to the plugin

--- src/main/java/io/kbamponsem/maven/util/NVMAwarePersistentClasses.java
+++ src/main/java/io/kbamponsem/maven/util/NVMAwarePersistentClasses.java
@@ -6,11 +6,11 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class NVMAwarePersistentClasses {
-    static public void getNVMAwarePersistentClasses(HashMap<Class, Boolean> persistentClasses, String basedir, String pInterface, MavenProject project, String copyClass){
+    static public void getNVMAwarePersistentClasses(HashMap<Class, Boolean> persistentClasses, String basedir, String pInterface, MavenProject project, String copyClass, String persistentAnnotation){
         persistentClasses.forEach((aClass, aBoolean) -> {
             if (aBoolean == true) {
                 try {
-                    byte[] bytes = Functions.transformClass(basedir + "/", aClass, pInterface, project, copyClass);
+                    byte[] bytes = Functions.transformClass(basedir + "/", aClass, pInterface, project, copyClass, persistentAnnotation);
 
                     String filename = aClass.getSimpleName();
 
